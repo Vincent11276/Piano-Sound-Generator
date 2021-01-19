d87c0e8b1d8f17b33f513da93c8b483d955c0c59
Vincent11276 <67948337+Vincent11276@users.noreply.github.com>
2021-01-19 16:37:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,5 @@
-import pygame
+import PianoPlayer as Piano
 import wx
-import time
-pygame.init()
-
-POLYPHONY = 64
-pygame.mixer.set_num_channels(POLYPHONY)
-channels = [pygame.mixer.Channel(i) for i in range(POLYPHONY)]
-
-
-class PianoPlayer:
-    def __init__(self, sound_pack_path='Resources/Piano Samples'):
-        self.min_value = 60
-        self.max_value = 72
-
-        self.sounds = []
-        for i in range(60, 73):
-            self.sounds.append(pygame.mixer.Sound(f'{sound_pack_path}/Piano {i}.mp3'))
-
-    def PlayNote(self, value):
-        if value < self.min_value or value > self.max_value:
-            return
-
-        for channel in channels:
-            if not channel.get_busy():
-                channel.play(self.sounds[value - self.min_value])
-                return
-
-        # If the code manages to get here, it means that all of the channels are occupied
-        global POLYPHONY
-        POLYPHONY += 1
-        pygame.mixer.set_num_channels(POLYPHONY)
-        channels.append(pygame.mixer.Channel(POLYPHONY - 1))
-        channels[-1].play(self.sounds[value])
 
 
 class App(wx.App):
@@ -56,7 +24,7 @@ class Frame(wx.Frame):
 class Panel(wx.Panel):
     def __init__(self, parent):
         super(Panel, self).__init__(parent=parent)
-        self.piano_player = PianoPlayer()
+        self.piano_player = Piano.PianoPlayer()
 
         self.value_lbl = wx.StaticText(parent=self, label="Note Value:", pos=(10, 12))
         self.noteValue_sCtrl = wx.SpinCtrl(parent=self, value="60", min=60, max=72, pos=(90, 10))
@@ -72,8 +40,3 @@ if __name__ == "__main__":
     App().MainLoop()
 
     piano = PianoPlayer()
-
-
-
-
-
